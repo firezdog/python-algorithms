d1f940685b9d7933479801437686d5190004f3aa
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-11-12 13:43:12 -0600

    filling out the API

--- Graphs/adjacency_matrix.py
+++ Graphs/adjacency_matrix.py
@@ -3,9 +3,10 @@ from Graphs.graph import Graph, Vertex
 
 
 class AdjacencyMatrix(Graph):
-    def __init__(self, vertices: int):
+    def __init__(self, vertices: List[Vertex]):
         self.cell_size = 6
-        self.vertices = [[False for y in range(vertices)] for x in range(vertices)]
+        self.vertices = vertices
+        self.matrix = [[0 for y in range(len(vertices))] for x in range(len(vertices))]
 
     def set_cell_size(self, cell_size: int) -> None:
         self.cell_size = cell_size
@@ -14,36 +15,51 @@ class AdjacencyMatrix(Graph):
     def add_vertex(self, vertex: Vertex) -> None:
         pass
 
-    def add_edge(self, from_vertex: Vertex, to_vertex: Vertex, weight: int) -> None:
-        pass
+    # easy mode for matrix: assume / require the keys are integers corr. to array position, otherwise we'd search
+    def add_edge(self, from_vertex: int, to_vertex: int, weight: int) -> None:
+        self.matrix[from_vertex][to_vertex] = weight
+        self.matrix[to_vertex][from_vertex] = weight
 
+    # by key
     def get_vertex(self, vertex_key: any) -> Vertex:
-        pass
+        for vertex in self.vertices:
+            if vertex.key == vertex_key:
+                return vertex
 
     def get_vertices(self) -> List[Vertex]:
-        pass
+        vertices = list()
+        return self.vertices
 
-    def __contains__(self, item: Vertex) -> bool:
-        pass
+    # by key
+    def __contains__(self, vertex_key: any) -> bool:
+        return self.get_vertex(vertex_key) is not None
 
     def render(self):
         rep = ' ' * self.cell_size
-        for i, row in enumerate(self.vertices):
-            col_header = 'V{}'.format(i)
+        for i, row in enumerate(self.matrix):
+            col_header = '{}'.format(self.vertices[i])
             rep += '{}{}'.format(col_header, ' ' * (self.cell_size - len(col_header)))
         rep += '\n'
-        for i, row in enumerate(self.vertices):
-            row_header = 'V{}'.format(i)
+        for i, row in enumerate(self.matrix):
+            row_header = '{}'.format(self.vertices[i])
             rep += '{}{}'.format(row_header, ' ' * (self.cell_size - len(row_header)))
             for edge in row:
                 rep += str(edge) + ' ' * (self.cell_size - len(str(edge)))
             rep += '\n'
-        return rep
+        print(rep)
 
 
 if __name__ == '__main__':
-    adjacency_matrix = AdjacencyMatrix(5)
-    adjacency_matrix.set_cell_size(10)
-    adjacency_matrix.vertices[0][0] = 'dog'
-    adjacency_matrix.vertices[0][3] = 'elephant'
-    print(adjacency_matrix.render())
+    sample_vertices = list()
+    for index in range(10):
+        new_vertex = Vertex(index, None)
+        sample_vertices.append(new_vertex)
+    adjacency_matrix = AdjacencyMatrix(sample_vertices)
+    adjacency_matrix.add_edge(3, 5, 1)
+    adjacency_matrix.add_edge(5, 3, 2)
+    adjacency_matrix.add_edge(4, 5, 1)
+    adjacency_matrix.render()
+    print(adjacency_matrix.get_vertex(3))
+    print(4 in adjacency_matrix)
+    print(12 in adjacency_matrix)
+    print(adjacency_matrix.get_vertices())
--- Graphs/graph.py
+++ Graphs/graph.py
@@ -6,6 +6,20 @@ class Vertex:
     def __init__(self, key: any, payload: any):
         self.key = key
         self.payload = payload
+        self.suppress_payload = True
+
+    def set_suppress_key(self, suppress_key: bool) -> None:
+        self.suppress_payload = suppress_key
+
+    def set_payload(self, payload: any) -> None:
+        self.payload = payload
+
+    def __str__(self):
+        return '{}:{}'.format(self.key, self.payload) if not self.suppress_payload \
+            else '{}'.format(self.key)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Graph(ABC):
@@ -14,7 +28,7 @@ class Graph(ABC):
         pass
 
     @abstractmethod
-    def add_edge(self, from_vertex: Vertex, to_vertex: Vertex, weight: int) -> None:
+    def add_edge(self, from_vertex: any, to_vertex: any, weight: int) -> None:
         pass
 
     @abstractmethod
