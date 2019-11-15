175c4163603812b50098c2d9cf5e676619101c1e
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-11-15 16:31:15 -0600

    removed interface bloat (for now); finalized adjacency_matrix and adding graph builder

--- Graphs/adjacency_matrix.py
+++ Graphs/adjacency_matrix.py
@@ -1,65 +1,45 @@
-from typing import List
-from Graphs.graph import Graph, Vertex
-
-
-class AdjacencyMatrix(Graph):
-    def __init__(self, vertices: List[Vertex]):
+class AdjacencyMatrix:
+    def __init__(self, num_vertices):
         self.cell_size = 6
-        self.vertices = vertices
-        self.matrix = [[0 for y in range(len(vertices))] for x in range(len(vertices))]
+        self.matrix = [[0 for y in range(num_vertices)] for x in range(num_vertices)]
+        self.edges = set()
 
-    def set_cell_size(self, cell_size: int) -> None:
+    def set_render_cell_size(self, cell_size):
         self.cell_size = cell_size
 
-    # requires adding an extra column to each row and an extra row to represent the new vertex
-    def add_vertex(self, vertex: Vertex) -> None:
-        pass
-
-    # easy mode for matrix: assume / require the keys are integers corr. to array position, otherwise we'd search
-    def add_edge(self, from_vertex: int, to_vertex: int, weight: int) -> None:
+    # will over-write existing edge
+    def add_edge(self, from_vertex, to_vertex, weight):
+        if type(weight) is not int or weight == 0:
+            raise ValueError("weight must be non-zero int")
         self.matrix[from_vertex][to_vertex] = weight
         self.matrix[to_vertex][from_vertex] = weight
+        self.edges.add((from_vertex, to_vertex))
+        self.edges.add((to_vertex, from_vertex))
 
-    # by key
-    def get_vertex(self, vertex_key: any) -> Vertex:
-        for vertex in self.vertices:
-            if vertex.key == vertex_key:
-                return vertex
+    def get_edges(self):
+        return self.edges
 
-    def get_vertices(self) -> List[Vertex]:
-        vertices = list()
-        return self.vertices
+    def get_edge(self, vertex_a, vertex_b):
+        # prob faster than iterating through edge list
+        return self.matrix[vertex_a][vertex_b] != 0
 
-    # by key
-    def __contains__(self, vertex_key: any) -> bool:
-        return self.get_vertex(vertex_key) is not None
+    def get_adjacent(self, vertex_a):
+        adjacent = list()
+        for vertex, weight in enumerate(self.matrix[vertex_a]):
+            if weight != 0:
+                adjacent.append(vertex)
+        return adjacent
 
     def render(self):
         rep = ' ' * self.cell_size
         for i, row in enumerate(self.matrix):
-            col_header = '{}'.format(self.vertices[i])
+            col_header = 'V{}'.format(i)
             rep += '{}{}'.format(col_header, ' ' * (self.cell_size - len(col_header)))
         rep += '\n'
         for i, row in enumerate(self.matrix):
-            row_header = '{}'.format(self.vertices[i])
+            row_header = 'V{}'.format(i)
             rep += '{}{}'.format(row_header, ' ' * (self.cell_size - len(row_header)))
             for edge in row:
                 rep += str(edge) + ' ' * (self.cell_size - len(str(edge)))
-            rep += '\n'
+            rep += '\n' if i < len(self.matrix) - 1 else ''
         print(rep)
-
-
-if __name__ == '__main__':
-    sample_vertices = list()
-    for index in range(10):
-        new_vertex = Vertex(index, None)
-        sample_vertices.append(new_vertex)
-    adjacency_matrix = AdjacencyMatrix(sample_vertices)
-    adjacency_matrix.add_edge(3, 5, 1)
-    adjacency_matrix.add_edge(5, 3, 2)
-    adjacency_matrix.add_edge(4, 5, 1)
-    adjacency_matrix.render()
-    print(adjacency_matrix.get_vertex(3))
-    print(4 in adjacency_matrix)
-    print(12 in adjacency_matrix)
-    print(adjacency_matrix.get_vertices())
---
+++ Graphs/build_graph.py
@@ -0,0 +1,5 @@
+import sys
+from Graphs.adjacency_matrix import AdjacencyMatrix
+
+if __name__ == '__main__':
+    file = sys.argv[1]
