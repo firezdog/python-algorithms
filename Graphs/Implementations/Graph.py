d347af31b1d8bf39724f6c93e4a18891fff94e84
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-12-12 21:30:05 -0600

    added labels

--- Graphs/Implementations/Graph.py
+++ Graphs/Implementations/Graph.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Set
+from typing import Set, cast
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -29,15 +29,22 @@ class Graph(ABC):
 
 def show_graph(graph: Graph) -> None:
     """ wrapper for module that builds and visualizes a standard graph """
+    from Graphs.Implementations.symbol_graph import SymbolGraph
     display_graph = nx.Graph()
     for vertex in range(graph.get_num_vertices()):
         display_graph.add_node(vertex)
     for edge in graph.get_edges():
         edge_list = [x for x in edge]
-        # for self loops
+        # for self loops -- this should no longer be a problem since I added checks to eliminate
         if len(edge_list) == 1:
             edge_list.append(edge_list[0])
         display_graph.add_edge(edge_list[0], edge_list[1])
-    nx.draw_circular(display_graph, node_size=400)
-    nx.draw_networkx_labels(display_graph, font_size=16, font_color='white', pos=nx.circular_layout(display_graph))
+    if isinstance(graph, SymbolGraph):
+        display_graph = nx.relabel_nodes(display_graph, graph.vertex_map)
+    pos = nx.circular_layout(display_graph)
+    custom_pos = {}
+    for k, v in pos.items():
+        custom_pos[k] = (v[0] - 0.2 * v[0], v[1] - 0.2 * v[1])
+    nx.draw(display_graph, pos)
+    nx.draw_networkx_labels(display_graph, custom_pos, font_color='red')
     plt.show()
